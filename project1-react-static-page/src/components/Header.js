66da7be6fb2ceb33b603c4bb8eb3887925df3079
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-10-23 16:21:14 +0800

    Organized components into a folder

--- project1-react-static-page/src/components/Header.js
+++ project1-react-static-page/src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header(){
     return (
       <nav className='nav'>
         <img src="./logo512.png" alt='react-logo' className='react-logo'/>
+        <h1>React Facts</h1>
         <ul className='nav-items'>
           <li>Pricing</li>
           <li>About</li>
--- project1-react-static-page/src/index.js
+++ project1-react-static-page/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // Import custom components
-import Header from './Header'
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header'
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 // Build page
 const page = (
