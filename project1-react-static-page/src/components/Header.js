108f9030b8018de6757e8d86e548e455f907b082
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-10-23 16:26:04 +0800

    Updated react logo directory and import
    Added an import of the react logo and organized it into the images folder.

--- project1-react-static-page/src/components/Header.js
+++ project1-react-static-page/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactLogo from '../images/react-logo.png';
 
 export default function Header(){
     return (
       <nav className='nav'>
-        <img src="./logo512.png" alt='react-logo' className='react-logo'/>
+        <img 
+          src={ReactLogo}
+          alt='react-logo' 
+          className='react-logo'
+        />
         <h1>React Facts</h1>
         <ul className='nav-items'>
           <li>Pricing</li>
