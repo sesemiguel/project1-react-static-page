d72b2aa56666ef8b194b71d517e591dab45afc48
Miguel Karlo Sese <sese.miguel9@gmail.com>
2022-10-23 17:57:19 +0800

    Added updates to complete the project
    Added designs and google font integration.

--- project1-react-static-page/src/components/Header.js
+++ project1-react-static-page/src/components/Header.js
@@ -4,17 +4,15 @@ import ReactLogo from '../images/react-logo.png';
 export default function Header(){
     return (
       <nav className='nav'>
-        <img 
-          src={ReactLogo}
-          alt='react-logo' 
-          className='react-logo'
-        />
-        <h1>React Facts</h1>
-        <ul className='nav-items'>
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <div className='logo-text-comb'>
+          <img 
+              src={ReactLogo}
+              alt='react-logo' 
+              className='react-logo'
+            />
+          <h1>ReactFacts</h1>
+        </div>
+        <h2 className="course-name">React Course - Project 1</h2>
       </nav>
     )
 }
